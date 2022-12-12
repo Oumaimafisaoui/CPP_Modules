6385aac1845f3d996889e4016d88dec30b85022a
Oumaimafisaoui <oumaimafisaoui@gmail.com>
2022-12-12 02:19:00 +0100

    e

--- c08/ex02/MutantStack.hpp
+++ c08/ex02/MutantStack.hpp
@@ -13,10 +13,12 @@ class MutantStack : public std::stack<T>
 
         MutantStack& operator=(const MutantStack &obj);
         
+        // use the underlying containcer iterators to iterate on stack
+
         typedef typename std::stack<T>::container_type::iterator iterator;
         typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-        
+        // code being and end for the iterators to return an iterator
 
 };
 
